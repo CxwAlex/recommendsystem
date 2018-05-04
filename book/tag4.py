a75376787f893808d010363c9af86a3b872b059a
CxwAlex <32626258+CxwAlex@users.noreply.github.com>
2018-05-04 16:30:17 +0800

    all_test_over

--- book/tag4.py
+++ book/tag4.py
@@ -1,20 +1,156 @@
+#更多需要思考的是关于标签的这套系统如何设计
+#输入数据：至少要有用户+商品+标签
+#后台数据要有：1输入的原始记录，2标签对应的总数目，3用户所打的标签与对应的次数，4物品被打的标签以及对应的次数
+#需要的功能：ETL（输入原始三元组，转换到所需格式——什么格式）；统计功能（离线or在线——存量与增量）
+#计算相似度功能：
+#用户的相似性：基于标签计算
+#物品的相似性：基于标签计算
+#标签的相似性：基于同一个物品计算&基于同一个用户计算
+#todo:统一使用json的格式或者统一使用三元组
+
+from pandas import Series,DataFrame
+from book.cf2 import list2matrix, count_set
+from collections import defaultdict
+import random
+import math
+#需要以下功能函数：
+#item_tag与tag_item互转
+#todo:是用什么数据结构来表示比较好，是统一表示还是利用不同的表格来关联，利用不同的表格关联吧
+#获取三元组数据的行和列
+def get_user_item_tag(train):
+    # etl模块，输入train，返回u，i，tag
+    # todo:如何解决顺序问题
+    u = set()
+    i = set()
+    t = set()
+    for a in train:
+        u.add(a[0])
+        i.add(a[1])
+        for tmp in a[2]:
+            t.add(tmp)
+    return u, i, t
+
+def get_user_tags(train):
+    # 获得user_tags矩阵
+    users, items, tags = get_user_item_tag(train)
+    result = DataFrame(0, index= tags, columns= users)
+    for i in train:
+        user = i[0]
+        tags = i[2]
+        for t in tags:
+            result[user][t] += 1
+    return result
+
+def get_item_tags(train):
+    # 获取item_tags矩阵
+    users, items, tags = get_user_item_tag(train)
+    result = DataFrame(0, index= tags, columns= items)
+    for i in train:
+        item = i[1]
+        tags = i[2]
+        for t in tags:
+            result[item][t] += 1
+    return result
+
+def get_user_item(train):
+    # 获取item_tags矩阵
+    users, items, tags = get_user_item_tag(train)
+    result = DataFrame(0, index= items, columns= users)
+    for i in train:
+        user = i[0]
+        item = i[1]
+        result[user][item] += 1
+    return result
+
+def count_tags(train):
+    # 返回每个标签出现的次数
+    tags = get_user_item_tag(train)[2]
+    result =Series(0, index= tags)
+    for i in train:
+        tags = i[2]
+        for t in tags:
+            result[t] += 1
+    return result
+
+
+def matrix2list(train):
+    return None
+
+def item_and_tag_to_dataframe(train):
+    # etl功能，将原始的list转换为dataframe
+    train = list2matrix(train)
+    return train
+
+def item_and_tag_transfrom(train):
+    # item和tag的相互转换
+    result = defaultdict(list)
+    if isinstance(train, DataFrame):
+        for i in train.index:
+            for j in train.columns[train.ix[i] != 0]:
+                result[i].append(j)
+    elif isinstance(train, dict):
+        train = list2matrix(train)
+        for i in train.index:
+            for j in train.columns[train.ix[i] != 0]:
+                result[i].append(j)
+
+    return result
+
+
+#根据user、item、tag为主键切分数据集
+# 该数据集的作用是，测试给用户关于某一物品推荐的标签是否和其真正打的标签一致
+# 输入的是原始数据集，返回的是分类好的训练集和测试集
+# 注意，此处采用的是随机分类，所以只有在数据量比较大的情况下，才可以得到比较符合设定的比例
+def SplitData(records):
+    # todo：该部分上不可用
+    # todo：需要原始格式的数据为user：item：tag格式
+    # 假设此处使用的是三元组格式
+    test = []
+    train = []
+    for record in records:
+            if random.randint(1, 10) == 1:
+                test.append(record)
+            else:
+                train.append(record)
+
+    return train, test
+
+
+
+
+#标签扩展：同一个物品的多个标签可能有一定的相似性
+#需要标签的相似度
+def recommend_tag():
+    return None
+
+
 #计算基于标签的物品余弦相似度
-def CosineSim(item_tags, i, j):
-    ret = 0
-    for b,wib in item_tags[i].items():
-        if b in item_tags[j]:
-            ret += wib * item_tags[j][b]
-    ni = 0
-    nj = 0
-    for b, w in item_tags[i].items():
-        ni += w * w
-    for b, w in item_tags[j].items():
-        nj += w * w
-    if ret == 0:
-        return 0
-    return ret / math.sqrt(ni * nj)
+#此处假设不包含标签的次数
+# todo:计算相似度时还要不要考虑打标签的次数
+def item_similarity(item_tags, i=None, j=None):
+    item_similarity = DataFrame(0.0, columns=item_tags.columns, index=item_tags.columns)
+    for item_i in item_tags.columns:
+        for item_j in item_tags.columns:
+            count = 0
+            if item_i == item_j:
+                continue
+            for tag in item_tags.index:
+                if item_tags[item_i][tag] == 0 or item_tags[item_j][tag] == 0:
+                    continue
+                count += 1
+            if count == 0:
+                item_similarity[item_i][item_j] = 0
+            else:
+                item_similarity[item_i][item_j] = count / math.sqrt(count_set(item_tags[item_i]) * count_set(item_tags[item_j]))
+
+    return item_similarity
+
+
 
 #利用相似度度量计算推荐列表的多样性
+# 如果推荐的物品相似度都很高，则说明多样性不强，反之，若相似度都不高，则说明多样性很高
+# todo：这个评测在什么时候做，针对单个用户或者用户全体做等
+# todo：暂时不能评测，因为推荐部分还没有完成——基于标签相似度的推荐
 def Diversity(item_tags, recommend_items):
     ret = 0
     n = 0
@@ -22,77 +158,64 @@ def Diversity(item_tags, recommend_items):
         for j in recommend_items.keys():
             if i == j:
                 continue
-            ret += CosineSim(item_tags, i, j)
+            ret += item_similarity(item_tags, i, j)
             n += 1
-    return ret / (n * 1.0)
+    #return 1 - ret / (n * 1.0)
+    return None
+
+
 
 #基于标签的推荐
-#用 records 存储标签数据的三元组，其中 records[i] = [user, item, tag] ;
-#用 user_tags  存储 n u,b ，其中 user_tags[u][b] =  n u,b ;
-#用 tag_items 存储 n b,i ，其中 tag_items[b][i] = n b,i 。
-
 #从 records 中统计出 user_tags 和 tag_items
-def InitStat(records):
-    user_tags = dict()
-    tag_items = dict()
-    user_items = dict()
-    for user, item, tag in records.items():
-        addValueToMat(user_tags, user, tag, 1)
-        addValueToMat(tag_items, tag, item, 1)
-        addValueToMat(user_items, user, item, 1)
-
 #统计出user_tags和tag_items之后，就可以通过以下程序对用户进行个性化推荐
-def Recommend(user):
-    recommend_items = dict()
-    tagged_items = user_items[user]
-    for tag, wut in user_tags[user].items():
-        for item, wti in tag_items[tag].items():
-            #if items have been tagged, do not recommend them
+def Recommend(train, user):
+    user_tags = get_user_tags(train)
+    tag_items = get_item_tags(train).T
+    user_items = get_user_item(train)
+    recommend_items = defaultdict(int)
+    tagged_items = user_items.index[user_items[user] != 0]
+
+    for tag in user_tags.index[user_tags[user] != 0]:
+        for item in tag_items.index[tag_items[tag] != 0]:
             if item in tagged_items:
                 continue
-            if item not in recommend_items:
-                recommend_items[item] = wut * wti
             else:
-                recommend_items[item] += wut * wti
+                recommend_items[item] += user_tags[user][tag] * tag_items[tag][item]
     return recommend_items
 
+
+
 #给用户推荐标签
+#todo:此处尚没有决定带有标签的数据集的形式，因此暂时不测试
 
 #给用户推荐整个系统里最热门的标签
-def RecommendPopularTags(user,item, tags, N):
-    return sorted(tags.items(), key=itemgetter(1), reverse=True)[0:N]
+def RecommendPopularTags(tags, N=1):
+    return tags.sort_values(ascending=False).index.tolist()[0:N]
+
 
 #给用户推荐物品i上最热门的标签
-def RecommendItemPopularTags(user,item, item_tags, N):
-    return sorted(item_tags[item].items(), key=itemgetter(1), reverse=True)[0:N]
+def RecommendItemPopularTags(item_tags, item=None, N=1):
+    if not item:
+        return None
+    return item_tags[item].sort_values(ascending=False).index.tolist()[0:N]
+
 
 #给用户推荐自己最经常会用的标签
-def RecommendUserPopularTags(user,item, user_tags, N):
-    return sorted(user_tags[user].items(), key=itemgetter(1), reverse=True)[0:N]
+def RecommendUserPopularTags(user_tags, user=None, N=1):
+    if not user:
+        return None
+    return user_tags[user].sort_values(ascending=False).index.tolist()[0:N]
 
 #结合上述两种方式
-def RecommendHybridPopularTags(user,item, user_tags, item_tags, alpha, N):
-    max_user_tag_weight = max(user_tags[user].values())
-    for tag, weight in user_tags[user].items():
-        ret[tag] = (1 – alpha) * weight / max_user_tag_weight
+#alpha是指用户自己经常打的标签的权重
+def RecommendHybridPopularTags(user,item, user_tags, item_tags, alpha = 0.3, N=1):
+    # 此处假设两者传来的标签一致
+    ret = Series(0.0, index= item_tags.index)
+    max_user_tag_weight = user_tags[user].max()
+    for tag in user_tags[user].index:
+        ret[tag] = (1 - alpha) * user_tags[user][tag] / max_user_tag_weight
+    max_item_tag_weight = item_tags[item].max()
+    for tag in item_tags[item].index:
+        ret[tag] += alpha * item_tags[item][tag] / max_item_tag_weight
 
-    max_item_tag_weight = max(item_tags[item].values())
-    for tag, weight in item_tags[item].items():
-        if tag not in ret:
-            ret[tag] = alpha * weight / max_item_tag_weight
-        else:
-            ret[tag] += alpha * weight / max_item_tag_weight
-    return sorted(ret[user].items(), key=itemgetter(1), reverse=True)[0:N]
-
-#根据user、item、tag为主键切分数据集
-def SplitData(records, train, test):
-    for user,item, tag in records:
-        if random.randint(1,10) == 1:
-            test.append([user,item,tag])
-        else:
-            train.append([user,item,tag])
-    return [train, test]
-
-
-
-
+    return ret.sort_values(ascending=False).index.tolist()[0:N]
--- test/test-4.py
+++ test/test-4.py
@@ -1,8 +1,7 @@
 # encoding=utf-8
 import unittest
 
-from book.coldstart3 import *
-
+from book.tag4 import *
 
 #构造原始数据集
 raw_data1 = {
@@ -13,17 +12,141 @@ raw_data1 = {
     "user5": ["刀剑神域", "龙泉刀剑", "天线宝宝"]
 }
 
-std_index = ["item1", "item2", "item3", "item4", "item5", "item6", "item7"]
+item_tag ={
+    "如何学习python":["python", "编程", "自学"],
+    "如何使用python编程":["python", "编程", "自学"],
+    "python进行数据分析":["python", "编程", "数据分析"],
+    "大数据处理":["大数据", "Hadoop", "Spark"],
+    "R语言数据分析":["R", "编程", "数据分析"],
+    "大数据数据分析":["大数据", "数据分析"],
+    "Hadoop核心技术":["Hadoop", "大数据"],
+    "Spark核心技术":["Spark", "大数据"],
+    "刀剑神域":["动画片", "刀剑"],
+    "龙泉刀剑":["刀剑", "商品"],
+    "天线宝宝":["动画片", "儿童"],
+    "微波炉菜谱":["做饭", "生活", "菜谱"],
+    "如何做饭":["做饭", "自学", "生活"]
+}
 
-std_data = raw2std(raw_data2, index= std_index)
+tag_item ={
+    "python":["如何学习python","如何使用python编程","python进行数据分析"],
+    "编程":["如何学习python","如何使用python编程","python进行数据分析","R语言数据分析"],
+    "自学":["如何学习python","如何使用python编程","如何做饭"],
+    "数据分析":["python进行数据分析","R语言数据分析","大数据数据分析"],
+    "大数据":["大数据处理","大数据数据分析","Hadoop核心技术","Spark核心技术"],
+    "R":["R语言数据分析"],
+    "Hadoop":["大数据处理","Hadoop核心技术"],
+    "Spark":["大数据处理","Spark核心技术"],
+    "动画片":["刀剑神域","天线宝宝"],
+    "刀剑":["刀剑神域","龙泉刀剑"],
+    "儿童":["天线宝宝"],
+    "做饭":["微波炉菜谱","如何做饭"],
+    "生活":["微波炉菜谱","如何做饭"],
+    "菜谱":["微波炉菜谱"]
+}
 
-class SimilarityTest(unittest.TestCase):
+user_item_tag = [
+    ["user1", "如何学习python", ["python", "编程", "自学"]],
+    ["user1", "如何使用python编程", ["python",  "编程"]],
+    ["user2", "Hadoop核心技术", ["Hadoop", "大数据"]],
+    ["user2", "python进行数据分析", ["python", "数据分析"]],
+    ["user2", "大数据数据分析", ["大数据", "数据分析"]],
+    ["user3", "刀剑神域", ["动画片", "刀剑"]],
+    ["user3", "如何学习python", ["python"]],
+    ["user3", "微波炉菜谱", ["做饭", "生活", "菜谱"]]
+]
 
-    def test_usersimilarity(self):
-        train = std_data
-        similarity = CalculateSimilarity(train)
-        self.assertEqual(similarity["user1"]["user5"], 0)
-        self.assertAlmostEqual(similarity["user1"]["user2"], 0.3333333)
+class ToolTest(unittest.TestCase):
+
+    def test_get_user_tags(self):
+        train = get_user_tags(user_item_tag)
+        #print("test_get_user_tags")
+        #print(train)
+
+        self.assertEqual(train["user2"]["数据分析"], 2)
+
+    def test_get_item_tags(self):
+        train = get_item_tags(user_item_tag)
+        #print("test_get_item_tags")
+        #print(train)
+
+        self.assertEqual(train["如何学习python"]["python"], 2)
+
+    def test_get_user_item(self):
+        train = get_user_item(user_item_tag)
+        #print("test_get_item_tags")
+        #print(train)
+
+        self.assertEqual(train["user3"]["微波炉菜谱"], 1)
+
+    def test_count_tags(self):
+        train = count_tags(user_item_tag)
+        #print("test_count_tags")
+        #print(train)
+
+        self.assertEqual(train["python"], 4)
+
+    def test_item_and_tag_to_dataframe(self):
+        train = item_and_tag_to_dataframe(item_tag)
+        train1 = item_and_tag_to_dataframe(tag_item)
+        #print("test_item_and_tag_to_dataframe")
+        #print(train)
+
+        self.assertEqual(train["如何学习python"]["python"], 1)
+        self.assertEqual(train1["python"]["如何学习python"], 1)
+
+    def test_item_and_tag_transform(self):
+        train = item_and_tag_transfrom(item_tag)
+        train1 = item_and_tag_transfrom(tag_item)
+        #print("test_item_and_tag_transform")
+        #print(train)
+
+        self.assertEqual(train['python'], ['如何学习python', '如何使用python编程', 'python进行数据分析'])
+        self.assertEqual(train1['如何学习python'], ['python', '编程', '自学'])
+
+
+    def test_split_data(self):
+        train,test = SplitData(user_item_tag)
+        #print("test_split_data")
+        #print(train)
+        #print(test)
+
+    def test_item_similarity(self):
+        item_tags = get_item_tags(user_item_tag)
+        item_sim = item_similarity(item_tags)
+        #print(item_sim)
+        self.assertEqual(item_sim['如何使用python编程']['python进行数据分析'], 0.5)
+
+    def test_Recommend(self):
+        recommend = Recommend(user_item_tag, 'user1')
+        self.assertEqual(recommend['python进行数据分析'], 2)
+
+class RecommendTagTest(unittest.TestCase):
+
+    def test_popular_tag(self):
+        tags = count_tags(user_item_tag)
+        rank = RecommendPopularTags(tags)
+        rank2 = RecommendPopularTags(tags, 2)
+        self.assertEqual(rank, ['python'])
+        self.assertEqual(rank2[0:1], ['python'])
+
+    def test_item_popular_tag(self):
+        item_tag = get_item_tags(user_item_tag)
+        rank = RecommendItemPopularTags(item_tag, '如何学习python')
+        self.assertEqual(rank, ['python'])
+
+    def test_user_popular_tag(self):
+        user_tag = get_user_tags(user_item_tag)
+        rank = RecommendUserPopularTags(user_tag, 'user1', 2)
+        self.assertEqual(set(rank), {'python', '编程'})
+
+    def test_hybrid_popular_tag(self):
+        item_tag = get_item_tags(user_item_tag)
+        user_tag = get_user_tags(user_item_tag)
+        result1 = RecommendHybridPopularTags('user2', '刀剑神域', user_tag, item_tag,N=2)
+        #print(set(result1))
+        self.assertAlmostEqual(set(result1), {'大数据', '数据分析'})
+
 
 if __name__ == '__main__':
     unittest.main()
